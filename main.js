f857b9df5b95dae0a6dbc5ad9ca00e1b6052c84e
jonathan03062011 <76205715+jonathan03062011@users.noreply.github.com>
2021-06-02 19:24:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftwristX=0;
+rightwristX=0;
+
 function preload()
 {
 
@@ -23,11 +29,22 @@ function gotposes(results)
     if(results.length>0)
     {
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nosex="+noseX+" ,nosey="+noseY);
+
+        leftwristX=results[0].pose.leftWrist.x;
+        rightwristX=results[0].pose.rightWrist.x;
+        difference=floor(leftwristX-rightwristX);
     }
 }
 
 
 function draw()
 {
- background('#34a8');
+ background('#34a832');
+ document.getElementById("square_side").innerHTML="Width and Height of the Square="+difference+"px";
+ fill('#3256a8');
+ stroke('#3256a8');
+ square(noseX,noseY,difference);
 }
